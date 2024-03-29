a2aa378b9335431c1556db4073a0093595fbc5c6
ryynnptr <116739553+ryynnptr@users.noreply.github.com>
2024-03-29 11:06:09 +0700

    Create strukturpegawai.cpp

---
+++ tugas lab/Tugas 2/strukturpegawai.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// struktur dari pegawai
+struct pegawai {
+    string nama;
+    string departemen;
+    int gaji;
+};
+
+// fungsi untuk menaikkan gaji
+void naikGaji(pegawai& pegawai1) {
+    pegawai1.gaji *= 1.10;
+}
+
+// objek dari pegawai1 sebagai output
+ int main(){
+    pegawai pegawai1;
+    pegawai1.nama = "Budi";
+    pegawai1.departemen = "IT";
+    pegawai1.gaji = 5000000;
+
+    naikGaji(pegawai1);
+
+cout << "Nama: " << pegawai1.nama << endl;
+cout << "Departemen: " << pegawai1.departemen << endl;
+cout << "Gaji: " << pegawai1.gaji << endl;
+
+return 0;
+ }
+
