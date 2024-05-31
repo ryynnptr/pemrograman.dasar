df45d7cbe4743631a3b23cc0eae6e43757ed8181
ryynnptr <116739553+ryynnptr@users.noreply.github.com>
2024-05-31 09:20:18 +0700

    Update iyan.cpp

--- praktikum10/iyan.cpp
+++ praktikum10/iyan.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
 
+using namespace std;
+
+class Number1 {
+    private :
+    int y ;
+
+    public :
+    void setY(int ui) {
+        y = ui;
+    }
+
+    int getY() {
+        return y;
+    }
+};
+
+class Number2 {
+   public :
+   int x ;
+};
+
+int main() {
+    Number1 satu ;
+    Number2 dua ;
+
+    satu.setY(25);
+    dua.x = 30;
+
+    cout << "Number Y: " << satu.getY() << endl;
+    cout << "Number X: " << dua.x << endl;
+
+    return 0;
+}
